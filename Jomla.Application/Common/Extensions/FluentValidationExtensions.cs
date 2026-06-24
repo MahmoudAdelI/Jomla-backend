3f7ae1a4db5f15a6135c408cbac2caafdc4a1f6d
Mahmoud Adel <mahmoud.adeli7888@gmail.com>
2026-06-24 16:46:24 +0300

    feat: added json validation extension rule for fluent validation

---
+++ Jomla.Application/Common/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System.Text.Json;
+
+namespace Jomla.Application.Common.Extensions
+{
+    public static class FluentValidationExtensions
+    {
+        public static IRuleBuilderOptions<T, string> IsValidJson<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(jsonString =>
+            {
+                if (string.IsNullOrWhiteSpace(jsonString))
+                    return false;
+
+                try
+                {
+                    using (JsonDocument.Parse(jsonString)) { }
+                    return true;
+                }
+                catch (JsonException)
+                {
+                    return false;
+                }
+            }).WithMessage("{PropertyName} contains an invalid JSON format.");
+        }
+    }
+}
