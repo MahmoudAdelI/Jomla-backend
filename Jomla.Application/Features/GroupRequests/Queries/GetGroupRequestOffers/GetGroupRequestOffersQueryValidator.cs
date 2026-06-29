ba21f292d7711e9a940a10a55bee3e84e7e017d7
Mahmoud Adel <mahmoud.adeli7888@gmail.com>
2026-06-29 05:10:38 +0300

    refactor: add validation rules for pagination Page/PageSize properties in query validator

--- Jomla.Application/Features/GroupRequests/Queries/GetGroupRequestOffers/GetGroupRequestOffersQueryValidator.cs
+++ Jomla.Application/Features/GroupRequests/Queries/GetGroupRequestOffers/GetGroupRequestOffersQueryValidator.cs
@@ -1,4 +1,4 @@
-﻿using FluentValidation;
+using FluentValidation;
 
 namespace Jomla.Application.Features.GroupRequests.Queries.GetGroupRequestOffers;
 
@@ -9,5 +9,13 @@ public sealed class GetGroupRequestOffersQueryValidator
     {
         RuleFor(x => x.GroupRequestId).NotEmpty()
             .WithMessage("Group request id is required.");
+
+        RuleFor(x => x.Page)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Page must be greater than or equal to 1.");
+
+        RuleFor(x => x.PageSize)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Page size must be greater than or equal to 1.");
     }
 }
