b3870e90cf557ca13a86daebfc24688acd577f8f
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2026-07-01 18:07:50 +0300

    feat: flexible accepted quantity on group request offer acceptance (#63)

--- Jomla.Domain/Entities/BuyerOfferResponse.cs
+++ Jomla.Domain/Entities/BuyerOfferResponse.cs
@@ -1,4 +1,4 @@
-﻿namespace Jomla.Domain.Entities
+namespace Jomla.Domain.Entities
 {
     public class BuyerOfferResponse
     {
@@ -7,6 +7,7 @@
         public Guid BuyerId { get; set; }
         public AppUser Buyer { get; set; } = null!;
         public BuyerOfferResponseType Response { get; set; }
+        public int AcceptedQuantity { get; set; }
         public string? StripePaymentIntentId { get; set; }
         public DateTime RespondedAt { get; set; }
     }
