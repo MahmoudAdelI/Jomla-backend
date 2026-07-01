364c3be3e5e78d458c4b7911065dfa99d0ad4182
Blackdukee <1213059@dtu.edu.eg>
2026-07-01 20:16:37 +0300

    feat: add FluentValidation rules for PlaceGroupRequestOffer command

--- Jomla.Application/Features/GroupRequests/Commands/PlaceGroupRequestOffer/PlaceGroupRequestOfferCommandValidator.cs
+++ Jomla.Application/Features/GroupRequests/Commands/PlaceGroupRequestOffer/PlaceGroupRequestOfferCommandValidator.cs
@@ -1,10 +1,4 @@
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Jomla.Application.Features.GroupRequests.Commands.PlaceGroupRequestOffer
 {
