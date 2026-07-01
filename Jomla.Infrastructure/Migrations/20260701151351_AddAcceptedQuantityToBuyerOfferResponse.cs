305eee75f561d89a28dc4c759cb5f313f579607d
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2026-07-01 18:14:03 +0300

    Feature/accept offer flexible quantity (#64)
    * feat: flexible accepted quantity on group request offer acceptance
    
    * db: add and apply EF migration to add AcceptedQuantity to BuyerOfferResponses

---
+++ Jomla.Infrastructure/Migrations/20260701151351_AddAcceptedQuantityToBuyerOfferResponse.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jomla.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAcceptedQuantityToBuyerOfferResponse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AcceptedQuantity",
+                table: "buyer_offer_responses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptedQuantity",
+                table: "buyer_offer_responses");
+        }
+    }
+}
